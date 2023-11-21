713d4076a0593c56919aa575cd5b973cc7444bb3
Echo <eeecho1998@gmail.com>
2023-11-21 14:06:42 -0600

    inital commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow} = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({show:false})
+    win.maximize()
+    win.loadFile('index.html')
+    win.removeMenu()
+    win.show()
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
